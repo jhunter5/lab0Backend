6c24cb8df1c0a2e4b113ba41ee8c3819e4208e7d
jhunter <jhunter@unal.edu.co>
2024-12-15 12:35:20 -0500

    agregado cors para nuestro front

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://lab0-limitless.vercel.app/', 'https://lab0-limitless.vercel.app/', 'http://localhost:3000'
+    origins 'https://lab0-limitless.vercel.app/', 'https://lab0-limitless.vercel.app', 'http://localhost:3000'
 
     resource '*',
       headers: :any,
