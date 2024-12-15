40cf46e02ceff806d3e7d8f244f3e5060f3b2e4a
jhunter <jhunter@unal.edu.co>
2024-12-15 12:33:06 -0500

    agregado cors para nuestro front

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://tu-front-end-domain.com', 'http://localhost:3000' # Cambia esto por el dominio del frontend y otros dominios necesarios
+    origins 'https://lab0-limitless.vercel.app/', 'https://lab0-limitless.vercel.app/', 'http://localhost:3000'
 
     resource '*',
       headers: :any,
